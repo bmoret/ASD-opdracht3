b4dea2b74ae32e150851996b50bc9c248891955f
ChromaChroma <jona.beer@gmail.com>
2021-10-08 11:52:17 +0200

    Add controller classes to session adapters

---
+++ sessioncontext/src/main/java/com/asd/session/port/adapter/api/rest/reservation/ReservationController.java
@@ -0,0 +1,7 @@
+package main.java.com.asd.session.port.adapter.api.rest.reservation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+}
---
+++ sessioncontext/src/main/java/com/asd/session/port/adapter/api/rest/session/SessionController.java
@@ -0,0 +1,7 @@
+package main.java.com.asd.session.port.adapter.api.rest.session;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+}

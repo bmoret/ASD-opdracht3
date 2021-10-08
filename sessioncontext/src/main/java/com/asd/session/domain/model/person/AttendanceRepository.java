0f655e0b84c9df7ed20d944fe62effb464be6cc2
ChromaChroma <jona.beer@gmail.com>
2021-10-08 11:44:07 +0200

    Add repository interface classes to session domain

---
+++ sessioncontext/src/main/java/com/asd/session/domain/model/person/AttendanceRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.asd.session.domain.model.person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, UUID> {
+}
